9def5cf63fa26b2100c20accec48ef0b01cc6e09
VgBraga <dev.victor.nascimento@gmail.com>
2026-07-29 13:28:40 -0300

    Criação da API

---
+++ Backend/google-music-api/src/main/java/com/googlemusic/api/GoogleMusicApiApplication.java
@@ -0,0 +1,13 @@
+package com.googlemusic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleMusicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleMusicApiApplication.class, args);
+	}
+
+}
