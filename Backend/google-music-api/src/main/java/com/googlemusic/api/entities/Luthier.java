e5fbe2a94f12158243cfe1d9196a1d147be3d183
MariaVitorino09 <dev.maria.vitorino@gmail.com>
2026-07-30 19:42:46 -0300

    backend finalizado

--- Backend/google-music-api/src/main/java/com/googlemusic/api/entities/Luthier.java
+++ Backend/google-music-api/src/main/java/com/googlemusic/api/entities/Luthier.java
@@ -1,9 +1,15 @@
 package com.googlemusic.api.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
@@ -26,6 +32,10 @@ public class Luthier {
 	protected String email;
 
 	protected String telefone;
+	
+	@OneToMany(mappedBy = "luthier")
+	@JsonIgnore 
+	private List<Instrumento> instrumentos = new ArrayList<>();
 
 	public Luthier() {
 
