06cb5d72fc73a41cacc5e29e9e82c98ca7cdb5e3
Maria Vitorino <215833404+MariaVitorino09@users.noreply.github.com>
2026-07-31 10:12:10 -0300

    Corrige MarcaService e MarcaRepository

--- Backend/google-music-api/src/main/java/com/googlemusic/api/repositories/MarcaRepository.java
+++ Backend/google-music-api/src/main/java/com/googlemusic/api/repositories/MarcaRepository.java
@@ -1,10 +1,9 @@
 package com.googlemusic.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import com.googlemusic.api.entities.Marca;
-
-
+@Repository
 public interface MarcaRepository extends JpaRepository<Marca, Long> {
 
 }
